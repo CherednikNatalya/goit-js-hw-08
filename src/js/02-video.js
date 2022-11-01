022d05cc12564ecc397b93f3c3ca801a990352ff
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-11-01 19:15:07 +0200

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,47 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const videoEl = document.querySelector('#vimeo-player');
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const CurrentTime = localStorage.getItem(LOCALSTORAGE_KEY)
+
+const player = new Player(videoEl);
+
+const onPlayData = function (data) {
+  save(LOCALSTORAGE_KEY, data);
+}
+
+player.on('timeupdate', function(data) {
+    console.log(data.seconds)
+localstorage.setItem()
+});
+
+player.setCurrentTime(30.456)
+
+
+player.setCurrentTime(CurrentTime).then(function(seconds) {
+  // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+      default:
+          // some other error occurred
+          break;
+  }
+});
+
+
+
+// const initPage = load(LOCALSTORAGE_KEY);
+// player.on('timeupdate', throttle(onPlayData, 1000));
+
+// if (initPage) {
+//   const { seconds } = initPage;
+//   player.setCurrentTime(seconds);
+// }
