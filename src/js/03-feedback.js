103a34b9841481539ac7c18fa3648c68e394e9b1
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-11-01 21:52:03 +0200

    4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const refs = {
+form: document.querySelector('.feedback-form'),
+textarea: document.querySelector('.feedback-form textarea'),
+}
+console.log(refs.form);
+console.log(refs.textarea);
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
 
+
+populateTexarea()
+
+function onTextareaInput (evt) {
+    const massage = evt.currentTarget.value;
+
+    localStorage.setItem('feedback-form-state', massage);
+}
+
+function onFormSubmit (evt) {
+    evt.preventDefault();
+    console.log('отправляем форму');
+    evt.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state')
+}
+
+function populateTexarea(){
+    const savedMAssage = localStorage.getItem('feedback-form-state')
+    if (savedMAssage){
+        refs.textarea.value = savedMAssage;
+    }
+}
