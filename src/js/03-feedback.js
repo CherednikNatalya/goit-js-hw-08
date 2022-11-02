369bb84156878f91541625e46fd59d53f7da74d5
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-11-02 10:46:43 +0200

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,18 +3,23 @@ import throttle from 'lodash.throttle'
 
 const STORAGE_KEY = 'feedback-form-state';
 
+const formData = {};
+
 const refs = {
 form: document.querySelector('.feedback-form'),
 textarea: document.querySelector('.feedback-form textarea'),
-}
+};
 
-console.log(refs.form);
-console.log(refs.textarea);
+
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
+refs.form.addEventListener('input', e => {
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
+});
 
-populateTexarea()
+populateTexarea();
 
 function onTextareaInput (evt) {
     const massage = evt.target.value;
@@ -25,7 +30,7 @@ function onTextareaInput (evt) {
 function onFormSubmit (evt) {
     evt.preventDefault();
     console.log('отправляем форму');
-    evt.target.reset();
+    evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY)
 }
 
